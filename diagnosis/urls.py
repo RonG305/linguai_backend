ccad98ef9d28c321ab7894bbfa83a5987dc92fdf
miriam <miriammugure610@gmail.com>
2024-11-14 09:41:27 +0300

    add the patients recommendations

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -3,6 +3,6 @@ from diagnosis import views
 
 urlpatterns = [
     path('diagnoses/all', views.get_diagnoses, name='diagnoses'),
-    path('dignoses/data/patient/', views.get_patient_diagnoses, name='patient_diagnoses') ,
+    path('diagnoses/data/patient/', views.get_patient_diagnoses, name='patient_diagnoses') ,
      
 ]
